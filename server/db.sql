192cdd59348039da3863d8a25bf1d303f22c5639
Naveen <naveenrajurenu03@gmail.com>
2024-05-26 20:41:58 +0530

    new commit

---
+++ server/db.sql
@@ -0,0 +1,20 @@
+-- CREATE TABLE users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     firstname VARCHAR(50) NOT NULL,
+--     lastname VARCHAR(50) NOT NULL,
+--     email VARCHAR(100) NOT NULL UNIQUE,
+--     password VARCHAR(255) NOT NULL,
+--     phonenumber VARCHAR(20) NOT NULL
+-- );
+
+
+-- CREATE TABLE Housedetails (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     housename VARCHAR(255) NOT NULL,
+--     place VARCHAR(255) NOT NULL,
+--     area VARCHAR(255) NOT NULL,
+--     bedrooms INT NOT NULL,
+--     bathrooms INT NOT NULL,
+--     nearbyplaces TEXT NOT NULL,
+--     houseimage VARCHAR(255) NOT NULL
+-- );
